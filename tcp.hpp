ec55244534c5a567d380c2bf93d0f6827d35e374
CatCoderJar <spaceredstoneminecrafted@gmail.com>
2026-03-03 19:56:09 +0200

    Refactor tcpServer and tcpClient methods

--- tcp.hpp
+++ tcp.hpp
@@ -7,6 +7,7 @@
 class tcpServer
 {
 private:
+	char buff[1000000]{};
 	WSADATA data;
 	ADDRINFO hints;
 	ADDRINFO* addrResult;
@@ -28,12 +29,12 @@ public:
 		WSACleanup();
 	}
 
-	int startServer()
+	int initServer()
 	{
 		if (WSAStartup(ver, &data) != 0)
 		{
-			std::cout << std::format("WSAStartup error, code:{}\n", WSAGetLastError());
-			return -1;
+			// std::cout << std::format("WSAStartup error, code:{}\n", WSAGetLastError());
+			return WSAGetLastError();
 		}
 
 		ZeroMemory(&hints, sizeof(hints));
@@ -44,52 +45,52 @@ public:
 
 		if (getaddrinfo(ip.c_str(), port.c_str(), &hints, &addrResult) != 0)
 		{
-			std::cout << std::format("Getaddrinfo error, code:{}\n", WSAGetLastError());
+			// std::cout << std::format("Getaddrinfo error, code:{}\n", WSAGetLastError());
 			WSACleanup();
 
-			return -1;
+			return WSAGetLastError();
 		}
 
 		ListenSocket = socket(addrResult->ai_family, addrResult->ai_socktype, (int)addrResult->ai_protocol);
 		if (ListenSocket == INVALID_SOCKET)
 		{
-			std::cout << std::format("Socket creation error error, code:{}\n", WSAGetLastError());
+			// std::cout << std::format("Socket creation error error, code:{}\n", WSAGetLastError());
 
 			freeaddrinfo(addrResult);
 			WSACleanup();
 
-			return -1;
+			return WSAGetLastError();
 		}
 
 		if (bind(ListenSocket, addrResult->ai_addr, addrResult->ai_addrlen) == SOCKET_ERROR)
 		{
-			std::cout << std::format("Bind error, code:{}\n", WSAGetLastError());
+			// std::cout << std::format("Bind error, code:{}\n", WSAGetLastError());
 
 			closesocket(ListenSocket);
 			freeaddrinfo(addrResult);
 			WSACleanup();
 
-			return -1;
+			return WSAGetLastError();
 		}
 
 		if (listen(ListenSocket, SOMAXCONN) == SOCKET_ERROR)
 		{
-			std::cout << std::format("Listen error, code:{}\n", WSAGetLastError());
+			// std::cout << std::format("Listen error, code:{}\n", WSAGetLastError());
 
 			freeaddrinfo(addrResult);
 			WSACleanup();
 
-			return -1;
+			return WSAGetLastError();
 		}
 
 		return 0;
 	}
-	int acceptSocket()
+	int acceptClient()
 	{
 		ClientSocket = accept(ListenSocket, NULL, NULL);
 		if (ClientSocket == INVALID_SOCKET)
 		{
-			std::cout << std::format("Accept error, code:{}\n", WSAGetLastError());
+			// std::cout << std::format("Accept error, code:{}\n", WSAGetLastError());
 
 			closesocket(ListenSocket);
 			freeaddrinfo(addrResult);
@@ -109,11 +110,10 @@ public:
 
 		return 0;
 	}
-	std::optional<std::string> readData() // Make as loop with thread, will return std::nullopt if connection will be closed
+	std::optional<std::string> readData() // Make as a cycle, better as thread, will return std::nullopt if connection will be closed
 	{
 		int res{ 0 };
-		char* buff = new char[1000000];
-		std::string data{};
+		std::optional<std::string> data{};
 
 		ZeroMemory(buff, sizeof(buff));
 		res = recv(ClientSocket, buff, 1000000, 0);
@@ -123,16 +123,21 @@ public:
 		}
 		data = buff;
 
-		delete[] buff;
-
-		return data;
+		if (data.has_value())
+		{
+			return data.value();
+		}
+		else
+		{
+			return std::nullopt;
+		}
 	}
 };
 
 class tcpClient
 {
 private:
-	char* buff = new char[1000000];
+	char buff[1000000]{};
 	std::string ip;
 	std::string port;
 	WSADATA data;
@@ -153,12 +158,12 @@ public:
 		WSACleanup();
 	}
 
-	int startClient()
+	int initClient()
 	{
 		if (WSAStartup(ver, &data) != 0)
 		{
-			std::cout << std::format("WSAStartup error, code:{}\n", WSAGetLastError());
-			return -1;
+			// std::cout << std::format("WSAStartup error, code:{}\n", WSAGetLastError());
+			return WSAGetLastError();
 		}
 
 		ZeroMemory(&hints, sizeof(hints));
@@ -166,37 +171,69 @@ public:
 		hints.ai_socktype = SOCK_STREAM;
 		hints.ai_protocol = IPPROTO_TCP;
 
-		if (getaddrinfo("localhost", "4096", &hints, &addrResult) != 0)
+		if (getaddrinfo(ip.c_str(), port.c_str(), &hints, &addrResult) != 0)
 		{
-			std::cout << std::format("Getaddrinfo error, code:{}\n", WSAGetLastError());
+			// std::cout << std::format("Getaddrinfo error, code:{}\n", WSAGetLastError());
 
 			freeaddrinfo(addrResult);
 			WSACleanup();
 
-			return -1;
+			return WSAGetLastError();
 		}
 
 		ConnectSocket = socket(addrResult->ai_family, addrResult->ai_socktype, (int)addrResult->ai_protocol);
 		if (ConnectSocket == INVALID_SOCKET)
 		{
-			std::cout << std::format("Socket creation error error, code:{}\n", WSAGetLastError());
+			// std::cout << std::format("Socket creation error error, code:{}\n", WSAGetLastError());
 			freeaddrinfo(addrResult);
 			WSACleanup();
-			return -1;
+			return WSAGetLastError();
 		}
 
 		return 0;
 	}
 
-	void connectToServer()
+	void connectLoop() // While isn't connected, trying to connect server
 	{
 		while (connect(ConnectSocket, addrResult->ai_addr, addrResult->ai_addrlen) == SOCKET_ERROR) { continue; }
-		std::cout << std::format("Connection established with at {} and port {}", ip, port);
+	}
+
+	int connectLoop(std::string_view ipArg, std::string_view portArg) // experimental
+	{
+		if (!addrResult)
+		{
+			freeaddrinfo(addrResult);
+		}
+		ZeroMemory(&hints, sizeof(hints));
+		if (getaddrinfo(ipArg.data(), portArg.data(), &hints, &addrResult) != 0)
+		{
+			return WSAGetLastError();
+		}
+		return connect(ConnectSocket, addrResult->ai_addr, addrResult->ai_addrlen);
+	}
+
+	int connectToServer() // just default connect
+	{
+		return connect(ConnectSocket, addrResult->ai_addr, addrResult->ai_addrlen);
+	}
+
+	int connectToServer(std::string_view ipArg, std::string_view portArg) // experimental
+	{
+		if (!addrResult)
+		{
+			freeaddrinfo(addrResult);
+		}
+		ZeroMemory(&hints, sizeof(hints));
+		if (getaddrinfo(ipArg.data(), portArg.data(), &hints, &addrResult) != 0)
+		{
+			return WSAGetLastError();
+		}
+		return connect(ConnectSocket, addrResult->ai_addr, addrResult->ai_addrlen);
 	}
 
 	int sendMsg(std::string message)
 	{
-		if (send(ConnectSocket, message.c_str(), message.length(), 0) != 0)
+		if (send(ConnectSocket, message.c_str(), message.length(), 0) == 0)
 		{
 			return -1;
 		}
@@ -204,10 +241,11 @@ public:
 		return 0;
 	}
 
-	std::optional<std::string> readData() // Make as a cycle, will return std::nullopt if connection will be closed
+	std::optional<std::string> readData() // Make as a cycle, better as thread, will return std::nullopt if connection will be closed
 	{
 		int res{ 0 };
-		std::string data{};
+		std::optional<std::string> data{};
+
 
 		ZeroMemory(buff, sizeof(buff));
 		res = recv(ConnectSocket, buff, 1000000, 0);
@@ -217,8 +255,13 @@ public:
 		}
 		data = buff;
 
-		delete[] buff;
-
-		return data;
+		if (data.has_value())
+		{
+			return data.value();
+		}
+		else
+		{
+			return std::nullopt;
+		}
 	}
 };
