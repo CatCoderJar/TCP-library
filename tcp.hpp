c11b087a9aa4938962cb073cae0c27267eb6578f
CatCoderJar <spaceredstoneminecrafted@gmail.com>
2026-03-23 20:17:57 +0200

    Refactor tcpServer and tcpClient for safety and flexibility

--- tcp.hpp
+++ tcp.hpp
@@ -1,5 +1,4 @@
 #define NOT_INITIALIZED -512
-
 #include <iostream>
 #include <Winsock2.h>
 #include <WS2tcpip.h>
@@ -10,6 +9,8 @@
 class tcpServer
 {
 private:
+	bool isSafeToContinue{ false };
+	char key{ 'h' };
 	char buff[1000000]{};
 	WSADATA data;
 	ADDRINFO hints;
@@ -17,21 +18,33 @@ private:
 	SOCKET ListenSocket{ INVALID_SOCKET };
 	SOCKET ClientSocket{ INVALID_SOCKET };
 	WORD ver{ MAKEWORD(2, 2) };
+	std::size_t clientsLimit{ SOMAXCONN };
 	std::string ip;
 	std::string port;
 public:
 	std::vector<SOCKET> clients{};
-	tcpServer(std::string_view ipArg, std::string_view portArg) : ip(ipArg), port(portArg)
+	tcpServer(std::string_view ipArg, std::string_view portArg, std::size_t clientsLimitArg = SOMAXCONN) : ip(ipArg), port(portArg), clientsLimit(clientsLimitArg)
 	{
 
 	}
 
 	void shutdownServer()
 	{
-		closesocket(ListenSocket);
+		if (!isSafeToContinue) { return; }
+		if (ListenSocket)
+		{
+			closesocket(ListenSocket);
+		}
+
+		for (std::size_t i{ 0 }; i < clients.size(); i++)
+		{
+			closesocket(clients[i]);
+		}
+
 		freeaddrinfo(addrResult);
 		WSACleanup();
 	}
+
 
 	int initServer()
 	{
@@ -48,7 +61,6 @@ public:
 
 		if (getaddrinfo(ip.c_str(), port.c_str(), &hints, &addrResult) != 0)
 		{
-			// std::cout << std::format("Getaddrinfo error, code:{}\n", WSAGetLastError());
 			WSACleanup();
 
 			return WSAGetLastError();
@@ -57,6 +69,8 @@ public:
 		ListenSocket = socket(addrResult->ai_family, addrResult->ai_socktype, (int)addrResult->ai_protocol);
 		if (ListenSocket == INVALID_SOCKET)
 		{
+			// std::cout << std::format("Socket creation error error, code:{}\n", WSAGetLastError());
+
 			freeaddrinfo(addrResult);
 			WSACleanup();
 
@@ -65,6 +79,7 @@ public:
 
 		if (bind(ListenSocket, addrResult->ai_addr, addrResult->ai_addrlen) == SOCKET_ERROR)
 		{
+
 			closesocket(ListenSocket);
 			freeaddrinfo(addrResult);
 			WSACleanup();
@@ -72,28 +87,25 @@ public:
 			return WSAGetLastError();
 		}
 
-		if (listen(ListenSocket, SOMAXCONN) == SOCKET_ERROR)
+		if (listen(ListenSocket, clientsLimit) == SOCKET_ERROR)
 		{
 			freeaddrinfo(addrResult);
 			WSACleanup();
 
 			return WSAGetLastError();
 		}
-
+		isSafeToContinue = true;
 		return 0;
 	}
-	int acceptClients(int clientsLimit = SOMAXCONN, sockaddr* addr = NULL, int addrLen = NULL) // loop, it will break if error and return error code, blocking function
+	int acceptClients(int clientsLimit = SOMAXCONN, sockaddr* addr = NULL, int* addrLen = NULL) // loop, it will break if error and return error code
 	{
+		if (!isSafeToContinue) { return NOT_INITIALIZED; }
 		for (std::size_t i{ 0 }; i <= clientsLimit; i++)
 		{
-			clients[i] = accept(ListenSocket, addr, NULL);
+			clients[i] = accept(ListenSocket, addr, addrLen);
 
 			if (clients[i] == INVALID_SOCKET)
 			{
-				closesocket(ListenSocket);
-				freeaddrinfo(addrResult);
-				WSACleanup();
-
 				return WSAGetLastError();
 			}
 		}
@@ -101,11 +113,14 @@ public:
 		return 0;
 	}
 
-	int acceptClient(sockaddr* addr = NULL, int addrLen = NULL)
+	int acceptClient(sockaddr* addr = NULL, int* addrLen = NULL)
 	{
-		ClientSocket = accept(ListenSocket, addr, &addrLen);
+		if (!isSafeToContinue) { return NOT_INITIALIZED; }
+		ClientSocket = accept(ListenSocket, addr, addrLen);
 		if (ClientSocket == INVALID_SOCKET)
 		{
+			// std::cout << std::format("Accept error, code:{}\n", WSAGetLastError());
+
 			closesocket(ListenSocket);
 			freeaddrinfo(addrResult);
 			WSACleanup();
@@ -116,23 +131,51 @@ public:
 		return 0;
 	}
 
-	int sendMsg(std::string message)
+	int sendMsg(std::string message, bool encrypt)
 	{
-		if (send(ClientSocket, message.c_str(), message.length(), 0) <= 0)
+		if (!isSafeToContinue) { return NOT_INITIALIZED; }
+		if (encrypt)
+		{
+			for (std::size_t i{ 0 }; i < message.size(); i++)
+			{
+				message[i] ^= key;
+			}
+		}
+		if (send(ClientSocket, message.data(), message.length(), 0) <= 0)
 		{
 			return WSAGetLastError();
 		}
 
 		return 0;
 	}
-	std::optional<std::string> readData() // Make as a cycle, better as thread, will return std::nullopt if connection will be closed
+
+	int sendMsgToSpecificClient(std::string message, std::size_t clientIndex, bool encrypt = false)
 	{
+		if (!isSafeToContinue) { return NOT_INITIALIZED; }
+		if (encrypt)
+		{
+			for (std::size_t i{ 0 }; i < message.size(); i++)
+			{
+				message[i] ^= key;
+			}
+		}
+		if (send(clients[clientIndex], message.c_str(), message.length(), 0) <= 0)
+		{
+			return WSAGetLastError();
+		}
+
+		return 0;
+	}
+
+	std::optional<std::string> readData(bool decrypt = false) // Make as a cycle, better as thread, will return std::nullopt if connection will be closed, or if server won't be initialized
+	{
+		if (!isSafeToContinue) { return std::nullopt; }
 		int res{ 0 };
 		std::optional<std::string> data{};
 
 		ZeroMemory(buff, sizeof(buff));
 		res = recv(ClientSocket, buff, 1000000, 0);
-		if (res == 0)
+		if (res == 0 || res < 0)
 		{
 			return std::nullopt;
 		}
@@ -140,6 +183,13 @@ public:
 
 		if (data.has_value())
 		{
+			if (decrypt)
+			{
+				for (std::size_t i{ 0 }; i < data.value().size(); i++)
+				{
+					data.value()[i] ^= key;
+				}
+			}
 			return data.value();
 		}
 		else
@@ -148,14 +198,15 @@ public:
 		}
 	}
 
-	std::optional<std::string> readDataFromSpecialClient(int clientNumber) // Make as a cycle, better as thread, will return std::nullopt if connection will be closed
+	std::optional<std::string> readDataFromSpecialClient(std::size_t clientIndex, bool decrypt = false) // Make as a cycle, better as thread, will return std::nullopt if connection will be closed, or if server won't be initialized
 	{
+		if (!isSafeToContinue) { return std::nullopt; }
 		int res{ 0 };
 		std::optional<std::string> data{};
 
 		ZeroMemory(buff, sizeof(buff));
-		res = recv(clients[clientNumber], buff, 1000000, 0);
-		if (res == 0)
+		res = recv(clients[clientIndex], buff, 1000000, 0);
+		if (res == 0 || res < 0)
 		{
 			return std::nullopt;
 		}
@@ -163,6 +214,13 @@ public:
 
 		if (data.has_value())
 		{
+			if (decrypt)
+			{
+				for (std::size_t i{ 0 }; i < data.value().size(); i++)
+				{
+					data.value()[i] ^= key;
+				}
+			}
 			return data.value();
 		}
 		else
@@ -175,7 +233,8 @@ public:
 class tcpClient
 {
 private:
-	bool isSafeToContinue{false};
+	bool isSafeToContinue{ false };
+	char key{ 'h' };
 	char buff[1000000]{};
 	std::string ip;
 	std::string port;
@@ -192,6 +251,7 @@ public:
 
 	void shutdownClient()
 	{
+		if (!isSafeToContinue) { return; }
 		closesocket(ConnectSocket);
 		freeaddrinfo(addrResult);
 		WSACleanup();
@@ -201,7 +261,6 @@ public:
 	{
 		if (WSAStartup(ver, &data) != 0)
 		{
-			isSafeToContinue = false;
 			return WSAGetLastError();
 		}
 
@@ -212,7 +271,6 @@ public:
 
 		if (getaddrinfo(ip.c_str(), port.c_str(), &hints, &addrResult) != 0)
 		{
-			isSafeToContinue = false;
 			freeaddrinfo(addrResult);
 			WSACleanup();
 
@@ -222,31 +280,67 @@ public:
 		ConnectSocket = socket(addrResult->ai_family, addrResult->ai_socktype, (int)addrResult->ai_protocol);
 		if (ConnectSocket == INVALID_SOCKET)
 		{
-			isSafeToContinue = false;
 			freeaddrinfo(addrResult);
 			WSACleanup();
 			return WSAGetLastError();
 		}
-		isSafeToContinue = true;
+
 		return 0;
 	}
 
-	int connectLoop() // While isn't connected, trying to connect server
+	void connectLoop() // While isn't connected, trying to connect server
 	{
-		if (!isSafeToContinue) { return NOT_INITIALIZED; }
+		if (!isSafeToContinue) { return; }
 		while (connect(ConnectSocket, addrResult->ai_addr, addrResult->ai_addrlen) == SOCKET_ERROR) { continue; }
 	}
 
-	int connectToServer() // just default connect, non blocking
+	int connectLoop(std::string_view ipArg, std::string_view portArg) // experimental
+	{
+		if (!isSafeToContinue) { return NOT_INITIALIZED; }
+		if (!addrResult)
+		{
+			freeaddrinfo(addrResult);
+		}
+		ZeroMemory(&hints, sizeof(hints));
+		if (getaddrinfo(ipArg.data(), portArg.data(), &hints, &addrResult) != 0)
+		{
+			return WSAGetLastError();
+		}
+		return connect(ConnectSocket, addrResult->ai_addr, addrResult->ai_addrlen);
+	}
+
+	int connectToServer() // just default connect
 	{
 		if (!isSafeToContinue) { return NOT_INITIALIZED; }
 		return connect(ConnectSocket, addrResult->ai_addr, addrResult->ai_addrlen);
 	}
 
-	int sendMsg(std::string_view message)
+	int connectToServer(std::string_view ipArg, std::string_view portArg) // experimental
 	{
 		if (!isSafeToContinue) { return NOT_INITIALIZED; }
-		if (send(ConnectSocket, message.data(), message.length(), 0) <= 0)
+		if (!addrResult)
+		{
+			freeaddrinfo(addrResult);
+		}
+		ZeroMemory(&hints, sizeof(hints));
+		if (getaddrinfo(ipArg.data(), portArg.data(), &hints, &addrResult) != 0)
+		{
+			return WSAGetLastError();
+		}
+		return connect(ConnectSocket, addrResult->ai_addr, addrResult->ai_addrlen);
+	}
+
+	int sendMsg(std::string message, bool encrypt)
+	{
+		if (!isSafeToContinue) { return NOT_INITIALIZED; }
+		if (encrypt)
+		{
+			for (std::size_t i{ 0 }; i < message.size(); i++)
+			{
+				message[i] ^= key;
+			}
+		}
+		if (send(ConnectSocket, message.data(), message.length(), 0) < 0)
 		{
 			return WSAGetLastError();
 		}
@@ -254,15 +348,15 @@ public:
 		return 0;
 	}
 
-	std::optional<std::string> readData() // Make as a cycle, better as thread, will return std::nullopt if connection will be closed or server isn't initialized
+	std::optional<std::string> readData(bool decrypt = false) // Make as a cycle, better as thread, will return std::nullopt if connection will be closed, or client isn't initialized
 	{
+		if (!isSafeToContinue) { return std::nullopt; }
 		int res{ 0 };
 		std::optional<std::string> data{};
-		if (!isSafeToContinue) { return std::nullopt; }
 
 		ZeroMemory(buff, sizeof(buff));
 		res = recv(ConnectSocket, buff, 1000000, 0);
-		if (res == 0)
+		if (res == 0 || res < 0)
 		{
 			return std::nullopt;
 		}
@@ -270,6 +364,13 @@ public:
 
 		if (data.has_value())
 		{
+			if (decrypt)
+			{
+				for (std::size_t i{ 0 }; i < data.value().size(); i++)
+				{
+					data.value()[i] ^= key;
+				}
+			}
 			return data.value();
 		}
 		else
